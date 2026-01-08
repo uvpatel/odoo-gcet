b30bef26a933b07b6d3cfb3594407376038d7c43
uvpatel7271 <uvpatel7271@gmail.com>
2026-01-08 08:37:48 +0530

    setup project

--- src/app/(admin)/page.tsx
+++
@@ -1,6 +0,0 @@
-//  Admin / HR Dashboard
-// ● Displays:
-// ○ Employee list
-// ○ Attendance records
-// ○ Leave approvals
-// ● Ability to switch between employees.
--- src/app/(root)/employee/dashboard/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-// Displays quick-access cards:
-// ○ Profile
-// 3. Functional Requirements
-// ○ Attendance
-// ○ Leave Requests
-// ○ Logout
-// Shows recent activity or alerts.
